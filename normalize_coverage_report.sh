20cbca6de4a3b0f6ffb4ecdb307518db121b78c0
Giovanni Sanchez <108043524+sisyphusSmiling@users.noreply.github.com>
2023-12-05 16:32:03 -0600

    add coverage normalization script

---
+++ normalize_coverage_report.sh
@@ -0,0 +1,5 @@
+sed -i 's/A.0000000000000007.StagedContractUpdates/contracts\/StagedContractUpdates.cdc/' coverage.lcov
+sed -i 's/A.0000000000000008.Foo/contracts\/Foo.cdc/' coverage.lcov
+sed -i 's/A.0000000000000009.A/contracts\/A.cdc/' coverage.lcov
+sed -i 's/A.0000000000000010.B/contracts\/B.cdc/' coverage.lcov
+sed -i 's/A.0000000000000010.C/contracts\/C.cdc/' coverage.lcov
