b0c19f50cb22952c113e1c2365c21c89c90104b1
Tom Haile <bradford.haile@gmail.com>
2024-02-14 08:23:08 -0600

    adding generated flix for transactions and scirpts, some failed, need to know if they are important

---
+++ gen_flix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the directories to traverse
+directories=("transactions" "scripts")
+
+# Iterate over each directory
+for directory in "${directories[@]}"; do
+    # Define the root directory for the current iteration
+    root_dir="./$directory"
+    
+    # Use find to search for all files in the directory and its subdirectories
+    find "$root_dir" -type f | while IFS= read -r filepath; do
+        # Call the flow executable with the filepath as a parameter
+        flow flix generate "$filepath" --save "$filepath.flix.json"
+    done
+
+    # Move all *.template.json files to the flix directory
+    find "./$directory" -type f -name "*.template.json" -exec mv {} "./flix" \;
+done
+
